0da7109c513c18b7756b26edd640451e6b4ce2d2
agz3001 <agz3001@yahoo.co.jp>
2024-02-09 00:57:03 +0900

    add taginput

---
+++ taginput.js
@@ -0,0 +1,50 @@
+// tgainput
+const tagContainer = document.querySelector(".tag-container")
+const input = document.querySelector(".tag-container input")
+let tags = []
+
+const createTag = (word) => {
+    const div = document.createElement("div")
+    div.setAttribute("class", "tag")
+    const span = document.createElement("span")
+    span.innerHTML = word
+    const closeBtn = document.createElement("i")
+    closeBtn.setAttribute("class", "material-icons")
+    closeBtn.setAttribute("data-item", word)
+    closeBtn.innerHTML = "close"
+
+    div.appendChild(span)
+    div.appendChild(closeBtn)
+
+    return div
+}
+
+const reset = () => {
+    const tagElements = document.querySelectorAll(".tag")
+    tagElements.forEach(v => v.remove())
+}
+
+const addTag = () => {
+    reset()
+    tags.slice().reverse().forEach(v => {
+        const inputs = createTag(v)
+        tagContainer.insertBefore(inputs, tagContainer.firstElementChild)
+    })
+}
+
+input.addEventListener("keyup", (event) => {
+    if (event.code === "Enter") {
+        tags.push(input.value)
+        addTag()
+        input.value = ""
+    }
+})
+
+document.addEventListener("click", (event) => {
+    if (event.target.tagName === "I") {
+        const value = event.target.getAttribute("data-item")
+        const index = tags.indexOf(value)
+        tags.splice(index, 1)
+        addTag()
+    }
+})
